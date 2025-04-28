24dafec298e5cea990b94ce208060268249f4b8c
Bork <kubecki.filip@gmail.com>
2025-04-28 20:19:42 +0200

    Preparation for lab

--- app/src/main/java/com/example/android_labolatorium/MainActivity.kt
+++ app/src/main/java/com/example/android_labolatorium/MainActivity.kt
@@ -1,12 +1,58 @@
 package com.example.android_labolatorium
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ListAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.get
+import androidx.core.view.marginBottom
+import androidx.transition.Visibility
 
 class MainActivity : AppCompatActivity() {
+//    Buttons
+    private lateinit var resetButton: Button
+    private lateinit var addButton: Button
+    private lateinit var subButton: Button
+
+//    Toggle Buttons
+    private lateinit var varA: ToggleButton
+    private lateinit var varB: ToggleButton
+    private lateinit var result: ToggleButton
+
+    private lateinit var gateImage: ImageView
+    private var currentOperator = LogicOperators.AND
+
+    private var list = listOf(
+        LogicOperators.AND,
+        LogicOperators.OR,
+        LogicOperators.NOT,
+        LogicOperators.NAND,
+        LogicOperators.NOR,
+        LogicOperators.XOR
+    )
+    private lateinit var spinner: Spinner
+
+    private var currentNumber: Int = 0
+
+    private lateinit var textView: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +62,101 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        addButton = findViewById(R.id.addButton)
+        subButton = findViewById(R.id.subButton)
+        resetButton = findViewById(R.id.resetButton)
+
+        varA = findViewById(R.id.varA)
+        varB = findViewById(R.id.varB)
+        result = findViewById(R.id.operationResult)
+
+        spinner = findViewById(R.id.spinner2)
+        val ad = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = ad
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                currentOperator = spinner.selectedItem as LogicOperators
+                gateImage.setImageResource(enumToImgResource(currentOperator))
+                if (currentOperator == LogicOperators.NOT){
+                    varB.visibility = INVISIBLE
+                }else{
+                    varB.visibility = VISIBLE
+                }
+                if (view != null) {
+                    onToggleClick(view)
+                }
+            }
+        }
+
+        gateImage = findViewById(R.id.gateImage)
+        gateImage.setBackgroundResource(R.drawable.and)
+
+
+        textView = findViewById(R.id.numberTextView)
+        textView.text = currentNumber.toString()
+
     }
+
+    private fun enumToImgResource(enum: LogicOperators): Int{
+        return when (enum){
+            LogicOperators.AND -> R.drawable.and
+            LogicOperators.NOT -> R.drawable.not
+            LogicOperators.OR -> R.drawable.or
+            LogicOperators.NAND -> R.drawable.nand
+            LogicOperators.NOR -> R.drawable.nor
+            LogicOperators.XOR -> R.drawable.xor
+        }
+    }
+
+    fun addOne(view: View) {
+        currentNumber += 1
+        textView.text = currentNumber.toString()
+    }
+
+    fun subOne(view: View) {
+        currentNumber -= 1
+        textView.text = currentNumber.toString()
+    }
+
+    fun reset(view: View) {
+        currentNumber = 0
+        textView.text = currentNumber.toString()
+    }
+
+    fun onToggleClick(view: View) {
+        val varA = varToBool(varA.text.toString())
+        val varB = varToBool(varB.text.toString())
+
+        result.text = when (currentOperator){
+            LogicOperators.NOT -> boolToToggleString(varA.not())
+            LogicOperators.AND -> boolToToggleString(varA and varB)
+            LogicOperators.OR -> boolToToggleString(varA or varB)
+            LogicOperators.NAND -> boolToToggleString(!(varA and varB))
+            LogicOperators.NOR -> boolToToggleString(!(varA or varB))
+            LogicOperators.XOR -> boolToToggleString(varA xor varB)
+        }
+    }
+
+    private fun varToBool(value: String): Boolean{
+        return if (value == "ON") true else {
+            if(value == "OFF"){
+                false
+            }else{
+                throw IllegalArgumentException("Wrong value")
+            }
+        }
+    }
+
+    private fun boolToToggleString(value: Boolean): String{
+        return if(value) "ON" else "OFF"
+    }
+}
+
+
+private enum class LogicOperators{
+    NOT, AND, OR, NAND, NOR, XOR
 }
