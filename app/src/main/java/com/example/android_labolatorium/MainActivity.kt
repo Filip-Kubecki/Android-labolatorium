572223e524cc617165b2d01fbc6e844025d19962
Bork <kubecki.filip@gmail.com>
2025-05-12 23:17:03 +0200

    Preparation for lab - PPK calc

--- app/src/main/java/com/example/android_labolatorium/MainActivity.kt
+++ app/src/main/java/com/example/android_labolatorium/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.android_labolatorium
 
 import android.annotation.SuppressLint
+import android.icu.text.DecimalFormat
+import android.media.MediaParser
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.SeekBar
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -16,31 +20,27 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlin.math.round
 import androidx.core.graphics.toColorInt
+import org.w3c.dom.Text
+import java.util.Locale
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
-//    Unit setting
-    private lateinit var weightUnitSpinner: Spinner
-    private lateinit var heightUnitSpinner: Spinner
+//  Minimalna krajowa brutto
+    private val minimalPay = 4666
+    private var currentGender = true // true male false female
 
-//    Number inputs
-    private lateinit var weightTextEdit: EditText
-    private var weightUnits = listOf(
-        WeightUnits.kg,
-        WeightUnits.lb
-    )
-    private var selectedWeightUnit = WeightUnits.kg
-    private lateinit var heightTextEdit: EditText
-    private val heightUnits = listOf(
-        HeightUnits.cm,
-        HeightUnits.inch,
-        HeightUnits.m
-    )
-    private var selectedHeightUnit = HeightUnits.cm
+    private lateinit var salary: TextView
+    private lateinit var employerPer: TextView
+    private lateinit var employeePer: TextView
+    private lateinit var age: TextView
+    private lateinit var gender: ImageView
+    private lateinit var savings: TextView
 
-//    Number output
-    private lateinit var bmiTextView: TextView
+    private lateinit var employerPerSeekBar: SeekBar
+    private lateinit var employeePerSeekBar: SeekBar
 
-    private lateinit var calcButton: Button
+
+
 
     @SuppressLint("MissingInflatedId", "NewApi", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,166 +53,95 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-//        Init input EditTexts
-        weightTextEdit = findViewById(R.id.editTextWeight)
-        heightTextEdit = findViewById(R.id.editTextHeight)
-//        heightTextEdit.setText(selectedHeightUnit.toString())
-
-//        Init Unit Spinners
-        weightUnitSpinner = findViewById(R.id.weightUnit)
-        val weightAdapter = ArrayAdapter(this, R.layout.simple_spinner_item, weightUnits)
-        weightAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
-        weightUnitSpinner.adapter = weightAdapter
-        weightUnitSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val currentVal = weightTextEdit.text.toString().toDouble()
-                val currentUnit = weightUnitSpinner.selectedItem as WeightUnits
-
-                weightTextEdit.setText(when(currentUnit){
-                    WeightUnits.kg -> {
-                        lbToKg(currentVal).round(2).toString()
-                    }
-                    WeightUnits.lb -> {
-                        kgToLb(currentVal).round(2).toString()
-                    }
-                })
-
-                selectedWeightUnit = weightUnitSpinner.selectedItem as WeightUnits
+        employerPerSeekBar = findViewById(R.id.employerSeekBar)
+        employerPerSeekBar.max = 25
+        employerPerSeekBar.min = 5
+        employerPerSeekBar.progress = 15
+        employerPerSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                employerPer.text = (employerPerSeekBar.progress/10.0).toString()
             }
-        }
 
-        heightUnitSpinner = findViewById(R.id.heightUnit)
-        val heightAdapter = ArrayAdapter(this, R.layout.simple_spinner_item, heightUnits)
-        heightAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
-        heightUnitSpinner.adapter = heightAdapter
-        heightUnitSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val currentVal = heightTextEdit.text.toString().toDouble()
-                val currentUnit = heightUnitSpinner.selectedItem as HeightUnits
-
-                if(selectedHeightUnit == HeightUnits.cm){
-                    heightTextEdit.setText(when (currentUnit) {
-                        HeightUnits.m -> {cmToM(currentVal).round(3).toString()}
-                        HeightUnits.inch -> {cmToInch(currentVal).round(3).toString()}
-                        else -> {"180.0"}
-                    })
-                }else if(selectedHeightUnit == HeightUnits.m){
-                    heightTextEdit.setText(when (currentUnit) {
-                        HeightUnits.cm -> {mToCm(currentVal).round(2).toString()}
-                        HeightUnits.inch -> {mToInch(currentVal).round(2).toString()}
-                        else -> {"180.0"}
-                    })
-                }else if(selectedHeightUnit == HeightUnits.inch){
-                    heightTextEdit.setText(when (currentUnit) {
-                        HeightUnits.cm -> {inchToCm(currentVal).round(3).toString()}
-                        HeightUnits.m -> {inchToM(currentVal).round(3).toString()}
-                        else -> {"180.0"}
-                    })
-                }
-
-                selectedHeightUnit = heightUnitSpinner.selectedItem as HeightUnits
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
             }
-        }
 
-//        Init output TextView
-        bmiTextView = findViewById(R.id.textViewBMI)
-        calcButton = findViewById(R.id.calculateButton)
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        employeePerSeekBar = findViewById(R.id.employeeSeekBar)
+        employeePerSeekBar.max = 25
+        employeePerSeekBar.min = 5
+        employeePerSeekBar.progress = 20
+        employeePerSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                employeePer.text = (employeePerSeekBar.progress/10.0).toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        salary = findViewById(R.id.salaryTextView)
+
+        employeePer = findViewById(R.id.employeePercent)
+        employerPer = findViewById(R.id.employerPercent)
+
+        savings = findViewById(R.id.savingsTextView)
+
+        age = findViewById(R.id.ageTextView)
+        gender = findViewById(R.id.genderImage)
+        gender.setBackgroundResource(R.drawable.male_symbol)
     }
 
     @SuppressLint("SetTextI18n")
-    fun calculateBMI(view: View) {
-        if((weightTextEdit.text.toString() == "") || (heightTextEdit.text.toString() == "")){
-            val errorPopup = Snackbar.make(view, R.string.errorNoInput, Snackbar.LENGTH_SHORT)
-            errorPopup.show()
+    fun subAge(view: View) {
+        age.text = (age.text.toString().toInt() - 1).toString()
+    }
 
-            bmiTextView.text = "Error"
+    @SuppressLint("SetTextI18n")
+    fun addAge(view: View) {
+        age.text = (age.text.toString().toInt() + 1).toString()
+    }
+
+    fun changeGender(view: View) {
+        currentGender = !currentGender
+        if (currentGender){
+            gender.setBackgroundResource(R.drawable.male_symbol)
         }else{
-            var weight = weightTextEdit.text.toString().toDouble()
-            var height = heightTextEdit.text.toString().toDouble()
-
-            if(weightUnitSpinner.selectedItem == WeightUnits.lb)
-                weight = lbToKg(weight)
-
-            height = when(heightUnitSpinner.selectedItem as HeightUnits){
-                HeightUnits.cm -> height/100
-                HeightUnits.m -> height
-                HeightUnits.inch -> inchToM(height)
-            }
-
-            val bmi =  (weight/(height*height)).round(2)
-
-            bmiTextView.text = bmi.round(2).toString()
-
-
-            if(bmi < 18){
-                bmiTextView.setBackgroundColor("#5ac1e9".toColorInt())
-                bmiTextView.setBackgroundResource(R.drawable.under_weight_circle)
-            }else if(bmi < 25){
-                bmiTextView.setBackgroundColor("#5ec982".toColorInt())
-                bmiTextView.setBackgroundResource(R.drawable.normal_weight_circle)
-            }else if(bmi < 30){
-                bmiTextView.setBackgroundColor("#ffd701".toColorInt())
-                bmiTextView.setBackgroundResource(R.drawable.overweight_weight_circle)
-            }else if(bmi < 40){
-                bmiTextView.setBackgroundColor("#fe8d00".toColorInt())
-                bmiTextView.setBackgroundResource(R.drawable.obese_weight_circle)
-            }else{
-                bmiTextView.setBackgroundColor("#ff0100".toColorInt())
-                bmiTextView.setBackgroundResource(R.drawable.morbidly_obese_weight_circle)
-                bmiTextView.setTextColor("#ffffff".toColorInt())
-            }
-
+            gender.setBackgroundResource(R.drawable.female_symbol)
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun calculatePpk(view: View) {
+        val ePer = employerPer.text.toString().toDouble()
+        val eePer = employeePer.text.toString().toDouble()
+        val salary = salary.text.toString().toDouble()
+        val age = age.text.toString().toInt()
+
+        val timeToPension = if (currentGender) 65 - age else 60 - age
+        val tmp = (((salary*ePer)/100+(salary*eePer)/100)*12*timeToPension)
+
+        savings.text = numberFormater(tmp)
+    }
+
+    fun numberFormater(value: Double):String{
+//        var tmp = ""
+//        var i = 0
+//        value.roundToInt().toString().forEach { it ->
+//            tmp = tmp+it
+//            if (i % 3 == 0){
+//                tmp = "$tmp "
+//            }
+//            i++
+//        }
+//
+//        return tmp
+        return value.toString()
     }
 }
 
-private fun Double.round(decimals: Int): Double {
-    var multiplier = 1.0
-    repeat(decimals) { multiplier *= 10 }
-    return round(this * multiplier) / multiplier
-}
-
-private enum class WeightUnits{
-    kg, lb;
-}
-
-private enum class HeightUnits{
-    cm, m, inch;
-}
-
-private fun kgToLb(value: Double): Double{
-    return value * 2.2046
-}
-
-private fun lbToKg(value: Double): Double{
-    return value * 0.453592
-}
-
-private fun cmToInch(value: Double): Double{
-    return value / 2.54
-}
-
-private fun inchToCm(value: Double): Double{
-    return value * 2.54
-}
-
-private fun cmToM(value: Double): Double{
-    return value * 0.01
-}
-
-private fun mToCm(value: Double): Double{
-    return value * 100
-}
-
-private fun inchToM(value: Double): Double{
-    return cmToM(inchToCm(value))
-}
-
-private fun mToInch(value: Double): Double{
-    return cmToInch(mToCm(value))
-}
